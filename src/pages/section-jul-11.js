91de30aa3b13b17bad58d305c623246ed3edf398
Waldo Lavaut <waldothedeveloper@gmail.com>
2021-07-22 15:18:57 -0400

    Adding detentions and arrests

--- src/pages/section-jul-11.js
+++ src/pages/section-jul-11.js
@@ -11,6 +11,8 @@ import SocialMediaEmbedCarousel from "../components/carousel"
 import { TwitterTweetEmbed } from "react-twitter-embed"
 import etecsa from "../images/ETECSA_logo.png"
 import list_of_detained from "../images/642-dissapeared-people.jpg"
+// import dinastars from "../images/dinastars.jpg"
+// import military_police from "../images/military-police-cuba.jpeg"
 
 const July11 = () => {
   return (
@@ -190,6 +192,67 @@ const July11 = () => {
                     wood sticks ready to beat anyone who would be peacefully
                     protesting:
                   </p>
+
+                  <div
+                    className="getty embed image"
+                    style={{
+                      backgroundColor: "#fff",
+                      display: "inline-block",
+                      fontFamily: "Roboto,sans-serif",
+                      color: "#a7a7a7",
+                      fontSize: 11,
+                      width: "100%",
+                      maxWidth: 594,
+                    }}
+                  >
+                    <div style={{ padding: 0, margin: 0, textAlign: "left" }}>
+                      <a
+                        href="http://www.gettyimages.com/detail/1233949092"
+                        target="_blank"
+                        style={{
+                          color: "#a7a7a7",
+                          textDecoration: "none",
+                          fontWeight: "normal !important",
+                          border: "none",
+                          display: "inline-block",
+                        }}
+                        rel="noreferrer noopener"
+                      >
+                        Embed from Getty Images
+                      </a>
+                    </div>
+                    <div
+                      style={{
+                        overflow: "hidden",
+                        position: "relative",
+                        height: 0,
+                        padding: "68.35017% 0 0 0",
+                        width: "100%",
+                      }}
+                    >
+                      <iframe
+                        src="//embed.gettyimages.com/embed/1233949092?et=E502nK7XT-VoAQgd3hRRHg&tld=com&sig=muvmo3T0bZnnnr2-ANHH1E26UjycBRYd8Cyvy2V8c8k=&caption=true&ver=1"
+                        scrolling="no"
+                        frameBorder={0}
+                        width={594}
+                        height={406}
+                        style={{
+                          display: "inline-block",
+                          position: "absolute",
+                          top: 0,
+                          left: 0,
+                          width: "100%",
+                          height: "100%",
+                          margin: 0,
+                        }}
+                      />
+                    </div>
+                  </div>
+
+                  <p className="py-6">
+                    A video of these gov officials carrying sticks was shared on
+                    Twitter:{" "}
+                  </p>
                   {/* twitter here */}
                   <TwitterTweetEmbed
                     tweetId="1414844174298353666"
@@ -197,16 +260,28 @@ const July11 = () => {
                   />
                 </div>
               </div>
-              <p className="py-12">
-                The videos and pictures shared on the internet from the
-                protesters themselves show the shocking level of violence of the
-                communist government&apos;s response.
+              <p className="pt-12">
+                Thousands of videos and pictures shared on the internet from the
+                protesters themselves shows a
               </p>
 
-              {/* This should be a carousel of instagram videos  */}
+              <span className="pt-12 pb-24 block text-3xl text-center leading-8 font-extrabold tracking-tight text-yellow-400 sm:text-4xl">
+                shocking level of violence from the government.
+              </span>
+
+              <p>Here are some of those shared on social media</p>
+
+              {/* videos shared by los Pichy Boys */}
               <SocialMediaEmbedCarousel data={pichyboysVideos} />
 
-              <blockquote>
+              <p className="py-12">
+                People are also afraid of taking out their phones on the streets
+                -even if it is to look at the time- thinking that if the police
+                catch them, they will think that they are making a video against
+                the government.
+              </p>
+
+              <blockquote className="py-12">
                 <p>
                   And we are ready for anything. They will have to pass over our
                   dead bodies if they think they can defeat the revolution
@@ -219,11 +294,11 @@ const July11 = () => {
             {/* section 3 */}
             <div className="space-y-12 mt-32 prose prose-blue prose-lg text-gray-300 mx-auto max-w-4xl">
               <p>
-                In addition to this, the Cuban government took an additional
-                measurement step:
+                In addition, the Cuban government took an extra step in response
+                to these events:
               </p>
 
-              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-yellow-400 sm:text-4xl">
+              <span className="py-12 block text-3xl text-center leading-8 font-extrabold tracking-tight text-yellow-400 sm:text-4xl">
                 Cutting the internet for the entire population of Cuba.
               </span>
 
@@ -255,13 +330,7 @@ const July11 = () => {
                 </p>
               </blockquote>
 
-              <p>
-                <span className="font-semibold">ETECSA</span> is the only phone
-                & internet company on the island. Which is in absolute control
-                of the government.
-              </p>
-
-              <figure className="flex justify-center">
+              <figure className="py-12 flex justify-center">
                 <img
                   className="w-auto h-auto object-cover"
                   src={etecsa}
@@ -270,11 +339,35 @@ const July11 = () => {
               </figure>
 
               <p>
-                People are also afraid of taking out their phones on the streets
-                -even if it is to look at the time- thinking that if the police
-                catch them, they will think that they are making a video against
-                the government.
+                <span className="text-yellow-400 underline">
+                  ETECSA is the only phone & internet company on the island.
+                </span>{" "}
+                But, this is not due to the U.S. embargo.
               </p>
+
+              <p>
+                Any internet & phone U.S. company -such as AT&T or T-Mobile- can
+                do business directly with Cuba.
+              </p>
+
+              <p>
+                The real problem is that the government{" "}
+                <span className="underline font-medium text-yellow-400">
+                  wants to control all communications on the island.
+                </span>{" "}
+                They want to see, hear, and know everything.
+              </p>
+
+              {/* twitter here */}
+              <TwitterTweetEmbed
+                tweetId="1416533421422039040"
+                placeholder="Loading"
+              />
+
+              <span className="text-gray-400 mt-1 text-base">
+                Julie Chung is the Acting Assistant Secretary for U.S.
+                Department of State&apos;s Bureau of Western Hemisphere Affairs.
+              </span>
             </div>
 
             {/* section 4 - people dissapeared */}
@@ -283,13 +376,168 @@ const July11 = () => {
               <div className="text-lg max-w-prose mx-auto">
                 <h1>
                   <span className="block text-base text-center text-red-600 font-semibold tracking-wide uppercase">
-                    events after July 11, 2021
+                    events July 12-22, 2021
                   </span>
                   <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-50 sm:text-4xl">
                     Missing and Detained People
                   </span>
                 </h1>
               </div>
+
+              <div
+                className="getty embed image"
+                style={{
+                  backgroundColor: "#fff",
+                  display: "inline-block",
+                  fontFamily: "Roboto,sans-serif",
+                  color: "#a7a7a7",
+                  fontSize: 11,
+                  width: "100%",
+                  maxWidth: 594,
+                }}
+              >
+                <div style={{ padding: 0, margin: 0, textAlign: "left" }}>
+                  <a
+                    href="http://www.gettyimages.com/detail/1233949303"
+                    target="_blank"
+                    style={{
+                      color: "#a7a7a7",
+                      textDecoration: "none",
+                      fontWeight: "normal !important",
+                      border: "none",
+                      display: "inline-block",
+                    }}
+                    rel="noreferrer"
+                  >
+                    Embed from Getty Images
+                  </a>
+                </div>
+                <div
+                  style={{
+                    overflow: "hidden",
+                    position: "relative",
+                    height: 0,
+                    padding: "63.468014% 0 0 0",
+                    width: "100%",
+                  }}
+                >
+                  <iframe
+                    src="//embed.gettyimages.com/embed/1233949303?et=r8odOetkQ2dZkP1NFRvuag&tld=com&sig=x0eDHfzBfPVzFKcyZfWM8sHhTsyAXM-drh8ePptrQhw=&caption=true&ver=1"
+                    scrolling="no"
+                    frameBorder={0}
+                    width={594}
+                    height={377}
+                    style={{
+                      display: "inline-block",
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      margin: 0,
+                    }}
+                  />
+                </div>
+              </div>
+
+              <p>
+                In response to the demonstrations, members of the National
+                Revolutionary Police, the Riot Squads, and the State Security
+                Department carried out arbitrary arrests and physical attacks
+                against people who were demonstrating peacefully, including
+                university students.
+              </p>
+
+              <p>
+                Similarly, people from different independent media outlets and
+                activists denounced the installation of security devices around
+                their homes to prevent them from going out onto the streets, a
+                practice that the Cuban authorities have previously adopted as a
+                tactic of intimidation.
+              </p>
+
+              {/* Anyelo Troya */}
+              <p>
+                <a
+                  target="_blank"
+                  href="https://twitter.com/AnyeloTroyaGonz"
+                  rel="noreferrer noopener"
+                >
+                  Anyelo Troya Gonzalez
+                </a>
+                , a Cuban photographer, was on the streets on July 11th filming
+                the protests. He was later detained and now sentenced to 1 year
+                in prison in a summary trial -without a lawyer- just for filming
+                the #11JCuba protests.
+              </p>
+
+              <TwitterTweetEmbed
+                tweetId="1417946236384993280"
+                placeholder="loading"
+              />
+
+              {/* Dina Stars */}
+              <p>
+                <a
+                  target="_blank"
+                  href="https://twitter.com/Dinastars_"
+                  rel="noopener noreferrer"
+                >
+                  Dina Stars
+                </a>
+                , a popular Cuban Youtuber and influencer was detained by
+                government security forces on{" "}
+                <a
+                  href="https://www.npr.org/2021/07/14/1015837541/a-popular-youtuber-is-detained-by-cuban-police-on-live-tv-in-a-roundup-of-critic"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  live television Tuesday morning
+                </a>
+                {` `}
+                as she was discussing the sweeping arrests of activists,
+                protesters and journalists.
+              </p>
+
+              <YoutubeEmbed embedId="S-gGqfryL-A" />
+
+              <blockquote>
+                <p>
+                  I hold the government responsible for anything that may happen
+                  to me
+                </p>
+              </blockquote>
+
+              <p>
+                the 25-year-old youtuber told the program&apos;s host before
+                abruptly leaving the interview.
+              </p>
+
+              <p>She added:</p>
+
+              <blockquote>
+                <p>I have to go. They told me to accompany them.</p>
+              </blockquote>
+
+              {/* 27NCuba */}
+              <p>
+                The 27NCuba movement is a group of independent Cuban artists,
+                writers, thinkers, and civil society members that formed in the
+                wake of the unjust arrest of rapper Denis Solis Gonzalez on
+                November 27, 2020.
+              </p>
+
+              <p>
+                Here&apos;s a video of their detention after peacefully
+                protesting in front of the Cuban Institute of Radio and
+                Television (ICRT in spanish)
+              </p>
+
+              {/* 27NCuba movemement */}
+              <TwitterTweetEmbed
+                tweetId="1414389085003784194"
+                placeholder="Loading"
+              />
 
               <img
                 className="h-auto w-auto object-cover"
