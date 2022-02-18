548a9aaa8eb080193e5a4f1c69ae4a6f0a8b6a8b
Waldo Lavaut <waldothedeveloper@gmail.com>
2022-02-17 23:08:20 -0500

    Added a few detault params to the faunadb config to see if this works

--- src/config/faunadb.js
+++ src/config/faunadb.js
@@ -2,6 +2,8 @@ import faunadb from "faunadb"
 const q = faunadb.query
 const FaunaDbClient = new faunadb.Client({
   secret: `${process.env.fauna_api_secret}`,
+  domain: `db.fauna.com`,
+  scheme: `https`,
 })
 
 export const saveProtestInDB = ({ data }) =>
